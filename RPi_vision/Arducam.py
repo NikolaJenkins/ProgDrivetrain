e14d2b7f9eaf4a5b0332cde2154a3b2bfcbfd27c
naojoy <njoy3663@student.pps.net>
2025-07-30 09:44:18 -0700

    image thresholding

--- RPi_vision/Arducam.py
+++ RPi_vision/Arducam.py
@@ -7,5 +7,7 @@ camera.setResolution(width, height)
 sink = cs.getVideo()
 while True:
    time, input_img = cvSink.grabFrame(input_img)
+   hsv_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2HSV)
+   binary_img = cv2.inRange(hsv_img, (min_hue, min_sat, min_val), (max_hue, max_sat, max_val))
    if time == 0: # There is an error
       continue
