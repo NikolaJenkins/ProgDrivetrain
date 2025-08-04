ba91ff45a64569ca0e577164a3921d799881c3d2
naojoy <njoy3663@student.pps.net>
2025-08-04 14:32:50 -0700

    nt

--- RPi_vision/cameraServer.py
+++ RPi_vision/cameraServer.py
@@ -1,15 +1,22 @@
 from cscore import CameraServer
-import networktables as nt
+from imutils.video import WebcamVideoStream
+import imutils
+from networktables import NetworkTable
 import time
 
-nt.NetworkTables.initialize(server = 'roborio-3636-frc.local')
-table = nt.NetworkTables.getTable('data table')
-x = table.getDoubleTopic('x').subscribe(0.0)
+# nt.NetworkTables.initialize(server = 'roborio-3636-frc.local')
+# table = nt.NetworkTables.getTable('data table')
+# x = table.getDoubleTopic('x').subscribe(0.0)
+NetworkTable.setTeam(3636)
+NetworkTable.setClientMode()
+NetworkTable.initialize()
+vp = NetworkTable.getTable("Stream")
+
 
 camera = CameraServer.startAutomaticCapture()
 CameraServer.enableLogging()
 
 while True:
     x += 1
-    table.putNumber('X', x)
+    vp.putNumber('X', x)
     time.sleep(1)
