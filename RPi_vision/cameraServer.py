e43676171c52c731194075de03157659c446c475
naojoy <njoy3663@student.pps.net>
2025-08-04 17:10:12 -0700

    fix changes

--- RPi_vision/cameraServer.py
+++ RPi_vision/cameraServer.py
@@ -33,8 +33,4 @@ mat = numpy.zeros(shape=(xResolution, yResolution, 3), dtype=numpy.uint8)
 # loop forever so program doesn't stop
 while True:
    time, mat = cvSink.grabFrame(mat)
-<<<<<<< HEAD
    outputStream.putFrame(mat)
-=======
-   #outputStream.putVideo(mat)
->>>>>>> b4598a060cdec0cb0dc9d904110dc63196eb6a37
