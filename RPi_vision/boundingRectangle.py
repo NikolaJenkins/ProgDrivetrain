7b92418cbe965fbf7c36e502e83852495ebc76b8
naojoy <njoy3663@student.pps.net>
2025-08-07 13:58:41 -0700

    nt fix

--- RPi_vision/boundingRectangle.py
+++ RPi_vision/boundingRectangle.py
@@ -57,8 +57,8 @@ def main():
 
         # Convert to HSV and threshold image
         hsv_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2HSV)
-        min_hue, min_sat, min_val = min_hsv_publisher.get()
-        max_hue, max_sat, max_val = max_hsv_publisher.get()
+        min_hue, min_sat, min_val = min_hsv_publisher.getEntry()
+        max_hue, max_sat, max_val = max_hsv_publisher.getEntry()
         binary_img = cv2.inRange(hsv_img, (min_hue, min_sat, min_val), (max_hue, max_sat, max_val))
         _, contour_list = cv2.findContours(binary_img, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)
         x_list = []
