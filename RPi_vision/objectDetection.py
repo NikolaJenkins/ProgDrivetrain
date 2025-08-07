6748ab4021c19215175bcf8ee71f160e826b3691
naojoy <njoy3663@student.pps.net>
2025-08-07 16:28:54 -0700

    object detection

--- RPi_vision/objectDetection.py
+++ RPi_vision/objectDetection.py
@@ -6,40 +6,31 @@ import numpy as np
 import time
 
 def main():
-    camera_config()
-    network_tables(True)
-    video_stream()
-    vision_table()
-    periodic()
-
-def camera_config():
+    is_table_host = True
+    
     with open('/boot/frc.json') as f:
         config = json.load(f)
     camera = config['cameras'][0]
     width = camera['width']
     height = camera['height']
-
-def network_tables(is_table_host):
     nt_instance = ntcore.NetworkTableInstance.getDefault()
+
     if is_table_host:
         nt_instance.startServer()
     else:
         nt_instance.setServerTeam(3636)
         nt_instance.startClient4("visionPi")
 
-def video_stream():
     CameraServer.startAutomaticCapture()
     input_stream = CameraServer.getVideo()
     output_stream = CameraServer.putVideo('Processed', width, height)
-
-def vision_table():
     vision_nt = nt_instance.getTable('Vision')
     img = np.zeros(shape=(240, 320, 3), dtype=np.uint8)
 
-def periodic():
     time.sleep(0.5)
+
     while True:
         time, input_image = input_stream.getFrame(img)
-    output_stream.putFrame(output_image)
+        output_stream.putFrame(output_stream)
 
 main()
