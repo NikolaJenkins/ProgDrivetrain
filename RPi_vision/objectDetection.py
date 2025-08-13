6c60b3cb4cd992344a68749a14833ef6c7ecdf41
naojoy <njoy3663@student.pps.net>
2025-08-12 18:29:04 -0700

    object detection

--- RPi_vision/objectDetection.py
+++ RPi_vision/objectDetection.py
@@ -4,31 +4,77 @@ import cv2
 import json
 import numpy as np
 import time
-def main():
-    is_table_host = True
 
-    with open('/boot/frc.json') as f:
-        config = json.load(f)
-    camera = config['cameras'][0]
-    width = camera['width']
-    height = camera['height']
+IS_TABLE_HOST = True
+
+# camera settings
+X_RESOLUTION = 320
+Y_RESOLUTION = 240
+FRAME_RATE = 30
+LINE_COLOR = (0, 255, 0)
+
+# object detection requirements
+MIN_COLOR_THRESHOLD = np.array([3, 80, 80])
+MAX_COLOR_THRESHOLD = np.array([6, 255, 255])
+MIN_CONTOUR_AREA = 400
+CONTOUR_RECT_THRESHOLD = 0.9
+
+# finds largest image within image with certain color
+def find_largest_object(hsv_image: np.ndarray) -> np.ndarray:
+    mask = cv2.inRange(hsv_image, MIN_COLOR_THRESHOLD, MAX_COLOR_THRESHOLD)
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if contours:
+        return max(contours, key = cv2.contourArea)
+    
+# checks if contour is shaped like object
+def contour_is_coral(contour: np.ndarray) -> bool:
+    if cv2.contourArea(contour) < MIN_CONTOUR_AREA:
+        return False
+    
+    # gets smallest convex polygon that fits around contour
+    contour_hull = cv2.convexHull(contour)
+    # gets smallest rectangle that fits around contour
+    rectangle = cv2.minAreaRect(contour_hull)
+    width = rectangle[1][0]
+    height = rectangle[1][1]
+    area = width * height
+    return cv2.contourArea(contour_hull) / area > CONTOUR_RECT_THRESHOLD
+
+def main():
+    # connect to server
     nt_instance = ntcore.NetworkTableInstance.getDefault()
 
-    if is_table_host:
+    # host or client?
+    if IS_TABLE_HOST:
         nt_instance.startServer()
     else:
         nt_instance.setServerTeam(3636)
         nt_instance.startClient4("visionPi")
 
+    # set up camera
     CameraServer.startAutomaticCapture()
     CameraServer.enableLogging()
     cv_sink = CameraServer.getVideo()
-    output_stream = CameraServer.putVideo('Processed', width, height)
+    output_stream = CameraServer.putVideo('Processed', X_RESOLUTION, Y_RESOLUTION)
+
+    # vision table
     vision_nt = nt_instance.getTable('Vision')
-    img = np.zeros(shape=(240, 320, 3), dtype=np.uint8)
+
+    img = np.zeros(shape=(X_RESOLUTION, Y_RESOLUTION, 3), dtype=np.uint8)
 
     while True:
+        # convert from bgr to hsv
         time, input_image = cv_sink.grabFrame(img)
+        frame_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        contour = find_largest_object(frame_hsv)
+
+        # draw rectangle on stream
+        if contour is not None and contour_is_coral(contour):
+            rect = cv2.minAreaRect(contour)
+            box = cv2.boxPoints(rect)
+            box = np.int0(box)
+            cv2.drawContours(img, [box], LINE_COLOR, 2)
+
         output_stream.putFrame(img)
 
 main()
